407f15dd870a86f23fdfebdfe82366400f3864ab
it24100264 <it24100264@my.sliit.lk>
2025-09-16 19:23:42 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24100264.R
@@ -0,0 +1,28 @@
+setwd("C:\\Users\\User\\Desktop\\IT24100163")
+
+##Question 01
+
+a <- 0
+b <- 40
+p <- (25-10) / (b-a)
+p
+
+
+##Question 02
+
+p <- pexp(2,rate = 1/3)
+p
+
+
+##Question 03
+
+##(i)
+p <- 1 - pnorm(130, mean = 100, sd = 15)
+p
+
+##(ii)
+q <- qnorm(0.95, mean = 100, sd = 15)
+q
+
+
+
